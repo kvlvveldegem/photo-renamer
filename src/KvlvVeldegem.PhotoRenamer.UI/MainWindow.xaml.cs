9928388c4578eac1d50d23140ab5158e06bb6397
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-09-16 14:54:12 +0200

    Implement UI logic

--- src/KvlvVeldegem.PhotoRenamer.UI/MainWindow.xaml.cs
+++ src/KvlvVeldegem.PhotoRenamer.UI/MainWindow.xaml.cs
@@ -1,28 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Avalon.Windows.Dialogs;
 
 namespace KvlvVeldegem.PhotoRenamer.UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly PhotoProcessor _photoProcessor = new PhotoProcessor();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnSelectFolderClicked(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var dialog = new FolderBrowserDialog
+            {
+                Title = "Select the folder containing all your photos"
+            };
+
+            var dialogOutput = dialog.ShowDialog();
+            if (dialogOutput == true)
+            {
+                FolderPath.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void OnStartProcessingClicked(object sender, RoutedEventArgs routedEventArgs)
+        {
+            ProcessPhotosInFolder();
+        }
+
+        private void ProcessPhotosInFolder()
+        {
+            var folderPath = FolderPath.Text;
+            if (Directory.Exists(folderPath) == false)
+            {
+                MessageBox.Show(this, $"Specified folder '{folderPath}' does not exist", "Folder does not exist", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                _photoProcessor.Rename(folderPath, AlbumName.Text);
+                MessageBox.Show(this, "All photos were renamed succesfully!", "Rename finished", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void OnAlbumNameTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            ValidateInputState();
+        }
+
+        private void ValidateInputState()
+        {
+            if (string.IsNullOrWhiteSpace(AlbumName.Text) == false && string.IsNullOrWhiteSpace(FolderPath.Text) == false)
+            {
+                StartProcessingButton.IsEnabled = true;
+            }
         }
     }
 }
---
+++ src/KvlvVeldegem.PhotoRenamer/PhotoProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace KvlvVeldegem.PhotoRenamer
+{
+    public class PhotoProcessor
+    {
+        /// <summary>
+        ///     Renames all the images in a specific folder
+        /// </summary>
+        /// <param name="folderPath">Folder containing all the photos to rename</param>
+        /// <param name="photoPrefix">Prefix of the photo</param>
+        public void Rename(string folderPath, string photoPrefix)
+        {
+            var photoUris = Directory.EnumerateFiles(folderPath)
+                .Where(file => file.ToLower().EndsWith(".png") || file.ToLower().EndsWith(".jpg"));
+
+            var counter = 1;
+
+            foreach (var photoUri in photoUris)
+            {
+                var newFileName = ComposeNewFileName(photoPrefix, counter, photoUri);
+                var newFileUri = $"{folderPath}\\{newFileName}";
+
+                File.Move(photoUri, newFileUri);
+
+                counter++;
+            }
+        }
+
+        private static string ComposeNewFileName(string photoPrefix, int counter, string photoUri)
+        {
+            var photoExtension = Path.GetExtension(photoUri).ToLower();
+            var newFileName = $"{photoPrefix} #{counter}{photoExtension}";
+            return newFileName;
+        }
+    }
+}
